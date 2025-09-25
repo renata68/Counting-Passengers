f168b4bc90bf2a10548abf5f8f0ebaf94aed7ab2
Renata Faltova <renata.97@seznam.cz>
2025-09-25 11:00:36 +0100

    styling css

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
 // document.getElementById("count-el").innerText = 10;
 
-// let count = 10
-
-// count = count + 1 // 10 + 1 = 11
-
-// console.log(count)
-
 // 9.
 // initialize the count as 0
 // listen for clicks in the increment button
@@ -15,26 +9,4 @@
 function increment() {
   console.log("The button was clicked")
 }
-// 10.
-// Create a function that logs out the number 42 to the console
-// Call/invoke the function
-
-function loggings() {
-  console.log(42)
-}
-
-loggings()
-
-// 11.
-let lap1 = 34
-let lap2 = 33
-let lap3 = 36
-
-// Create a function that logs out the sum of all the lap times
-function logLapTime() {
-  let totalTime = lap1 + lap2 + lap3
-  console.log(totalTime)
-}
-
-logLapTime()
 
