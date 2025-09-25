f3c630d7d4b8de45299bbccc377ccee95f013e6e
Renata Faltova <renata.97@seznam.cz>
2025-09-25 09:35:25 +0100

    add comments

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-document.getElementById("count-el").innerText = 10
+// document.getElementById("count-el").innerText = 10;
+
+// let firstBatch = 5
+
+// let secondBatch = 7
+
+// let count = firstBatch + secondBatch
+
+// // tohle se pouziva hlavne pro debugovani
+// console.log(count)
+
+
+// playing with js
+// let myAge = 28
+// let humanDogRatio = 7
+
+// let myDogAge = myAge * humanDogRatio
+// console.log(myDogAge)
