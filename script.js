6fc41b6791d0baa14fdbe95e1cc0df914e3c21fa
Renata Faltova <renata.97@seznam.cz>
2025-09-26 08:34:00 +0100

    add save button

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-// // camelCase
-// let countEL = document.getElementById("count-el")
-
-// console.log(countEl) // it is a model of HTML element
-
-// let count = 0 //we have to start with 0 increment the count variable when the button is clicked
-
-
-// function increment() {
-//   // console.log("clicked!")
-//   count = count + 5
-//   countEl.innerText = count
-//   // console.log(count)
-// }
-
 let countEL = document.getElementById("count-el")
 let count = 0
 
